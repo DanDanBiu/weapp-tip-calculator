032eb7d0bc41a07879b71698b935c5353246927c
dandanbiu <dandanbiubiu@gmail.com>
2017-01-11 23:50:20 -0800

    Add WeUI components
    1. Add WeUI components
    2. Add split feature

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,45 +3,56 @@ Page({
   data: {
     focus: true,
     inputValue: 0,
-    array: ['15%', '18%', '20%', '25%'],
+    array: ['Fair - 15%', 'Good - 18%', 'Outstanding - 20%', 'Perfect - 25%'],
     index: 0,
     percents: [0.15, 0.18, 0.2, 0.25],
     tips: 0,
-    total: 0
+    total: 0,
+    split: 1
   },
-  bindPickerChange: function(e) {
+  bindPickerChange: function (e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
       index: e.detail.value
     })
   },
-  bindKeyInput: function(e) {
+  bindKeyInput: function (e) {
     console.log('用户输入，携带值为', e.detail.value)
     this.setData({
       inputValue: e.detail.value
     })
   },
-  formSubmit: function(e) {
+  sliderchange: function (e) {
+    console.log('用户输入，携带值为', e.detail.value)
     this.setData({
-      tips: this.data.inputValue * this.data.percents[this.data.index],
+      split: e.detail.value
+    })
+  },
+  submit: function (e) {
+    this.setData({
+      tips: this.data.inputValue * this.data.percents[this.data.index]
+    })
+    this.setData({
       total: +this.data.inputValue + +this.data.tips
     })
+    console.log('tips', this.data.tips)
+    console.log('total', this.data.total)
+    var tipPerPerson = (this.data.tips / this.data.split).toFixed(2).toString()
+    var totalPerPerson = (this.data.total / this.data.split).toFixed(2).toString()
     wx.showModal({
       title: '计算结果',
-      content: '小费：' + tips.toString() + '\n' + '总金额：' + total.toString(),
+      content: '每人小费：$ ' + tipPerPerson + '\n' + '每人总金额：$ ' + totalPerPerson,
       showCancel: false,
-      success: function(res) {
+      success: function (res) {
         if (res.confirm) {
           console.log('用户点击确定')
         }
       }
     })
-    //this.formReset
   },
-  formReset: function(e) {
+  reset: function (e) {
     this.setData({
       index: 0,
-      inputValue: 0,
       tips: 0,
       total: 0
     })
